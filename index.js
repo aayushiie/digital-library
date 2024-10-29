0a51c4d36967ab480afb40233a1f2f188d12395d
Aayushi Singh <aayushi.singh067@gmail.com>
2024-10-29 07:16:51 +0530

    created author routes, template files

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 
 const indexRouter = require('./routes/main');
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -25,5 +26,6 @@ db.once('open', () => {
 })
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000);
